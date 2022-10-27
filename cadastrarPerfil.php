3029343edfba8bf68afc8926f539e77c8030cf3e
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-10-27 16:58:31 -0300

    Add files via upload

--- cadastrarPerfil.php
+++ cadastrarPerfil.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
   $nome = $_REQUEST["username"];
   $hashsenha = md5($_REQUEST["senha"]);
   $sql =<<<EOF
-     INSERT INTO public.perfil (nome, email ,curso_idcurso ,hashsenha)
+     INSERT INTO public.Pessoa (nome, email ,curso_idcurso ,hashsenha)
      VALUES ('$nome','$hashsenha' );
 EOF;
   $ret = pg_query($db, $sql);
