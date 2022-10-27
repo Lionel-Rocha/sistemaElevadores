5e2d84ec189a8b264fc598aec05319bc9dcedd89
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-10-27 18:50:13 -0300

    Add files via upload

---
+++ buscarRota.php
@@ -0,0 +1,28 @@
+<?php
+include("conexao.php");
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
+  $endereco = $_GET["endereco"];
+  $sql =<<<EOF
+  select * from rota where endereco = '$endereco'
+  
+EOF;
+  $ret = pg_query($db, $sql);
+  if(!$ret) {
+     http_response_code(501);
+  } else {
+     http_response_code(200);
+     $response["rota"] = array();
+    while($row = pg_fetch_row($ret)) {
+      $rota = array();
+      $rota["rotaid"] = $row[0];
+      $rota["contrato"] = $row[1];
+      $rota["edificio"] = $row[2];
+      $rota["ordem"] = $row[3];
+      $rota["unirdade"] = $row[4];
+      array_push($response["rota"], $rota);
+    }
+    echo json_encode($response);
+  }
+}
+
+?>
