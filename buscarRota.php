c39e2cc095f5c2a4e26d7d9bd44072a21a4af8aa
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-10-27 20:24:45 -0300

    Add files via upload

--- buscarRota.php
+++ buscarRota.php
@@ -20,9 +20,8 @@ EOF;
       // $rota["endereco"] = $row[4];
       // $rota["unidade"] = $row[5];
       array_push($response["dados"], $rota);
-      echo $response;
     }
-    echo $ret;
+    echo $response;
     // echo json_encode($response);
     // header("Location: https://sistemaelevadores.herokuapp.com/rota.html");
   }
