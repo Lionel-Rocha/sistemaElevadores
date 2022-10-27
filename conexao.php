ba6bca4ea2db9f6650d394bcc95932a045bd877b
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-10-27 16:36:49 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -5,7 +5,6 @@ $dbname      = "dbname = d150o1hra5udor";
 $credentials = "user = dnkjvdrahpifir password=96eb96960fa8074df89cde9089eb23a61bf56b62ee25dd16f04a95da1d3e941f";
 $db = pg_connect( "$host $port $dbname $credentials"  );
 	if(!$db) {
-	      echo $host
 	      echo "Error : Unable to open database\n";
 	 } else {
 	      echo "Opened database successfully\n";
