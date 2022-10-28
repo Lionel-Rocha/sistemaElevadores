95ccd81e43c0a890a0815fa8ba78101d467aad9c
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-10-28 00:12:47 -0300

    Add files via upload

--- buscarRota.php
+++ buscarRota.php
@@ -11,27 +11,39 @@ EOF;
   if(!$ret) {
      http_response_code(501);
   } else {
-     http_response_code(200);
-     $response["dados"] = array();
+        echo "<table>
+    <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+    <th>Hometown</th>
+    <th>Job</th>
+    </tr>";
     while($row = pg_fetch_row($ret)) {
-      $rota = array();
-      $rota["rotaid"] = $row[0];
-      $rota["contrato"] = $row[1];
-      $rota["edificio"] = $row[2];
-      $rota["ordem"] = $row[3];
-      $rota["endereco"] = $row[4];
-      $rota["unidade"] = $row[5];
-      array_push($response["dados"], $rota);
+      echo "<tr>";
+      echo "<td>" . $row[0] . "</td>";
+      echo "<td>" . $row[1] . "</td>";
+      echo "<td>" . $row[2] . "</td>";
+      echo "<td>" . $row[3] . "</td>";
+      echo "<td>" . $row[4] . "</td>";
+      echo "</tr>";
+    }
+    echo "</table>";
+    //  http_response_code(200);
+    //  $response["dados"] = array();
+    // while($row = pg_fetch_row($ret)) {
+    //   $rota = array();
+    //   $rota["rotaid"] = $row[0];
+    //   $rota["contrato"] = $row[1];
+    //   $rota["edificio"] = $row[2];
+    //   $rota["ordem"] = $row[3];
+    //   $rota["endereco"] = $row[4];
+    //   $rota["unidade"] = $row[5];
+    //   array_push($response["dados"], $rota);
 
 
     }
-    // echo json_encode($response);
-    printf($rota["rotaid"]);
-    printf($rota["contrato"]);
-    printf($rota["edificio"]);
-    printf($rota["ordem"]);
-    printf($rota["endereco"]);
-    printf($rota["unidade"]);
+    //echo json_encode($response);
 
 
 
